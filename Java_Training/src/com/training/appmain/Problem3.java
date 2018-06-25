f99cc8a616e79c2906a75fc3922086ef32486533
prajapatiswapnil <prajapati.swapnil@gmail.com>
2018-06-26 01:07:30 +0530

    Problem3

---
+++ Java_Training/src/com/training/appmain/Problem3.java
@@ -0,0 +1,52 @@
+package com.training.appmain;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+
+		int a[] = { 0, 1, 2, 2, 3, 5 };
+		int b[] = { 500000, 500000, 0, 0, 0, 20000 };
+
+		System.out.println(solution(a, b));
+
+	}
+
+	public static int solution(int[] A, int[] B) {
+		int len = A.length;
+		float C[] = new float[len];
+		int pairs = 0;
+		for (int i = 0; i < len; i++) {
+			C[i] = (float) (A[i] + B[i] / 1000000.00);
+		}
+
+		// for (float x: C) System.out.println(x);
+
+		for (int p = 0; p < len; p++) {
+			for (int q = p + 1; q < len; q++) {
+
+				if ((C[p] * C[q]) >= (C[p] + C[q])) {
+
+					if (pairs > 1000000000)
+						return 1000000000;
+					else
+						pairs++;
+
+					// System.out.println("(" + p + "," + q +") " + C[p] + " * " + C[q]+ "=" + C[p]
+					// * C[q] + " " + C[p] + " + " + C[q]+ "=" + (C[p]+C[q]));
+				}
+
+			}
+		}
+
+		return pairs;
+
+	}
+
+
+
+}
